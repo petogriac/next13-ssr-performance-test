0e93d593f6e799ee9fd5d0fc029d905f4301914d
petoGriac <peter.griac@garwan.sk>
2023-09-08 15:35:21 +0200

    fetching data

--- app/l/[count]/page.tsx
+++ app/l/[count]/page.tsx
@@ -1,10 +1,44 @@
+'use client';
+
 import Filter from '@/components/filter/filter';
 import ArticleItem from '@/components/article-item/article-item';
 import './listing.scss';
-import data from '@/data.json';
+import { useEffect, useState } from 'react';
 
 export default function Listing({params}: { params: { count: string } }) {
-    const articles = data.content;
+    const [data,setData]: [
+        data: any,
+        setData: any
+    ]=useState([]);
+    const getData= async ()=>{
+        // await new Promise(resolve => setTimeout(resolve, +params.count));
+
+        fetch('/data.json'
+            ,{
+                headers : {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        )
+            .then(function(response){
+                console.log(response)
+                return response.json();
+            })
+            .then(function(myJson) {
+                console.log(myJson);
+                setData(myJson.content)
+            });
+    }
+
+    useEffect(()=>{
+        getData();
+    },[])
+
+
+    if(!data){
+        return <div>Loading...</div>
+    }
 
     return (
         <div>
@@ -14,7 +48,7 @@ export default function Listing({params}: { params: { count: string } }) {
 
             <div className='listing-items'>
                 {
-                    articles.map(article => (
+                    data.map((article: any) => (
                         <ArticleItem
                             key={article.id}
                             id={article.id}
@@ -26,4 +60,18 @@ export default function Listing({params}: { params: { count: string } }) {
             </div>
         </div>
     )
+}
+
+async function getData() {
+    const res = await fetch('data.json')
+
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
 }
